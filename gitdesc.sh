ee15ad706d611bf8645050842e9dfda694454c75
Peter Hyman <pete@peterhyman.com>
2021-01-03 08:40:33 -0600

    Add tagdate command to provide the commit date of a tag

--- gitdesc.sh
+++ gitdesc.sh
@@ -15,8 +15,9 @@ usage() {
 cat >&2  <<EOF
 $(basename $0) command [-r]
 all - entire git describe
-commit - commit, omitting v
+commit - commit, omitting g
 tagrev - tag revision count
+tagdate - date tag committed
 major - major release version
 minor - minor release version
 micro - micro release version
@@ -41,6 +42,9 @@ commit=
 
 # count of commits from last tag
 tagrev=
+
+# date tag committed
+tagdate=
 
 # major version
 major=
@@ -72,8 +76,12 @@ commit_length=7
 # with leading v and commit leadint g removed
 
 init() {
+	local realtag
 	# git describe raw format
 	describe_tag=$(git describe $tagopt --long --abbrev=$commit_length)
+
+	# grab real tag version in case we need it for git-show
+	realtag=$(echo $describe_tag | cut -d- -f1)
 
 	# if tag has a leading `v' this will remove
 	# if some other tag format is used, change or omit
@@ -82,13 +90,24 @@ init() {
 	# git describe prefixes commit with the letter `g'
 	# this substitution removes the g. If the letter `g' is part of tag, this logic
 	# will need revision, such as reversing $describe_tag and then reverting
-	# echo $describe | rev, for example
+	# echo $describe | rev, for example, and get variables right to left
 	describe_tag=${describe_tag/g/}
 
 	# assign commit, tag revision, and version to variables using `-' separator
 	commit=$(echo $describe_tag | cut -d- -f3)
 	tagrev=$(echo $describe_tag | cut -d- -f2)
 	version=$(echo $describe_tag | cut -d- -f1)
+
+	# if tag date is needed
+	# alter date format per strftime formats
+	# YYYY-mm-dd
+	tagdate=$(git show -n1 -s --date=format:"%Y-%m-%d" --format="%cd" $realtag)
+
+	# (Other examples)
+	# dd mmm YYYY
+	# tagdate=$(git show -n1 -s --date=format:"%d %b %Y" --format="%cd" $version)
+	# dd MMMMMMM YYYY
+	# tagdate=$(git show -n1 -s --date=format:"%d %B %Y" --format="%cd" $version)
 
 	# set micro version or full micro version if tag revision > 0
 	micro=$(echo $version | cut -d. -f3)
@@ -125,6 +144,9 @@ case "$1" in
 		;;
 	"tagrev" )
 		retval=$tagrev
+		;;
+	"tagdate" )
+		retval=$tagdate
 		;;
 	"version" )
 		retval=$version
